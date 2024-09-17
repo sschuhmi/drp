363fbb78a8afff8738f88d6f8245e870cc327d93
sschuhmi <stephan.schuhmann@gmx.de>
2024-09-17 07:48:51 +0200

    Updated run.py - added visualizations

--- app/run.py
+++ app/run.py
@@ -43,6 +43,26 @@ def index():
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    # Descending categorical message counts for ALL messages (for 2nd visualization):
+    cats =  df[df.columns[4:]] # select categories within DataFrame
+    cat_counts = (cats.shape[0] * cats.mean()).sort_values(ascending=False)
+    cat_names = list(cat_counts.index)
+    
+    # Descending categorical message counts only for NEWS messages (for 3nd visualization):
+    cats_news =  df[df.genre == 'news'].drop('id', axis=1) # select NEWS genre within df, drop ID col
+    cat_news_counts = (cats_news.shape[0] * cats_news.mean()).sort_values(ascending=False)
+    cat_news_names = list(cat_news_counts.index)
+    
+    # Descending categorical message counts only for DIRECT messages (for 4th visualization):
+    cats_dir =  df[df.genre == 'direct'].drop('id', axis=1) # select DIRECT genre within df, drop ID col
+    cat_dir_counts = (cats_dir.shape[0] * cats_dir.mean()).sort_values(ascending=False)
+    cat_dir_names = list(cat_dir_counts.index)
+    
+    # Descending categorical message counts only for SOCIAL messages (for 5th visualization):
+    cats_soc =  df[df.genre == 'social'].drop('id', axis=1) # select SOCIAL genre within df, drop ID col
+    cat_soc_counts = (cats_soc.shape[0] * cats_soc.mean()).sort_values(ascending=False)
+    cat_soc_names = list(cat_soc_counts.index)
+    
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -57,12 +77,88 @@ def index():
             'layout': {
                 'title': 'Distribution of Message Genres',
                 'yaxis': {
-                    'title': "Count"
+                    'title': "Message Count"
                 },
                 'xaxis': {
                     'title': "Genre"
                 }
             }
+        },
+        # Plotting of messages according to categories in descending order (2nd visualization)
+        {
+            'data': [
+                Bar(
+                    x=cat_names,
+                    y=cat_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Message Category Distribution - ALL messages',
+                'yaxis': {
+                    'title': "Message Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+        },
+        # ...filtered on NEWS (3rd visualization)
+        {
+            'data': [
+                Bar(
+                    x=cat_news_names,
+                    y=cat_news_counts
+                )
+            ],
+        
+            'layout': {
+                'title': 'Distribution of Message Categories - only NEWS messages',
+                'yaxis': {
+                    'title': "Message Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }     
+        },
+        # ...filtered on DIRECT (4th visualization)
+        {
+            'data': [
+                Bar(
+                    x=cat_dir_names,
+                    y=cat_dir_counts
+                )
+            ],
+        
+            'layout': {
+                'title': 'Distribution of Message Categories - only DIRECT messages',
+                'yaxis': {
+                    'title': "Message Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }     
+        },
+        # ...filtered on SOCIAL (5th visualization)
+        {
+            'data': [
+                Bar(
+                    x=cat_soc_names,
+                    y=cat_soc_counts
+                )
+            ],
+        
+            'layout': {
+                'title': 'Distribution of Message Categories - only SOCIAL messages',
+                'yaxis': {
+                    'title': "Message Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }     
         }
     ]
     
