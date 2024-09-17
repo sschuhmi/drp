1bfbec6b383ae6cddd7595ea8d4d5f7c0003a77c
sschuhmi <stephan.schuhmann@gmx.de>
2024-09-17 09:36:34 +0200

    Update train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -88,7 +88,7 @@ def build_model():
     model = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
-        ('clf', MultiOutputClassifier(AdaBoostClassifier(algorithm='SAMME.R', base_estimator=None, learning_rate=1.0, n_estimators=50, random_state=None))) 
+        ('clf', MultiOutputClassifier(AdaBoostClassifier(algorithm='SAMME.R', learning_rate=1.0, n_estimators=50, random_state=None))) 
     ])
   
     return model
